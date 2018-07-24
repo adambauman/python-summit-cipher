15c2196b55b50f5003b190e6fe91ef3eb11fde3a
Adam Bauman <adam@kungfutreachery.net>
2018-07-23 22:33:26 -0500

    Added basic sound

--- gs_summit_pygame.py
+++ gs_summit_pygame.py
@@ -9,6 +9,7 @@ g_fullscreen = False
 g_matrix_output_pins = [4,17,27,22,5]
 g_matrix_input_pins = [18,23,24,25,12]
 g_cipher_panels = []
+g_sound_library = {}
 
 # NOTE: (Adam) Initialize GPIO
 try:
@@ -24,55 +25,90 @@ except:
     print("GPIO setup error, exiting")
     GPIO.cleanup()
     quit()
+
+
+def LoadSounds():
+    g_sound_library["success"] = pygame.mixer.Sound("success_trimmed.ogg")
     
 
 def LoadCipherPanels():
     g_cipher_panels.append(
-        {'active_image': pygame.image.load("red_plate_active.png"),
-         'inactive_image': pygame.image.load("red_plate_inactive.png"),
-         'is_active': False,
-         'starting_pos': (0,0)
+        {"active_image": pygame.image.load("red_plate_active.png"),
+         "inactive_image": pygame.image.load("red_plate_inactive.png"),
+         "is_active": False,
+         "past_state": False,
+         "starting_pos": (0,0)
          })
     g_cipher_panels.append(
-        {'active_image': pygame.image.load("orange_plate_active.png"),
-         'inactive_image': pygame.image.load("orange_plate_inactive.png"),
-         'is_active': False,
-         'starting_pos': (384,0)
+        {"active_image": pygame.image.load("orange_plate_active.png"),
+         "inactive_image": pygame.image.load("orange_plate_inactive.png"),
+         "is_active": False,
+         "past_state": False,
+         "starting_pos": (384,0)
          })
     g_cipher_panels.append(
-        {'active_image': pygame.image.load("green_plate_active.png"),
-         'inactive_image': pygame.image.load("green_plate_inactive.png"),
-         'is_active': False,
-         'starting_pos': (768,0)
+        {"active_image": pygame.image.load("green_plate_active.png"),
+         "inactive_image": pygame.image.load("green_plate_inactive.png"),
+         "is_active": False,
+        "past_state": False,
+         "starting_pos": (768,0)
          })
     g_cipher_panels.append(
-        {'active_image': pygame.image.load("blue_plate_active.png"),
-         'inactive_image': pygame.image.load("blue_plate_inactive.png"),
-         'is_active': False,
-         'starting_pos': (1152,0)
+        {"active_image": pygame.image.load("blue_plate_active.png"),
+         "inactive_image": pygame.image.load("blue_plate_inactive.png"),
+         "is_active": False,
+         "past_state": False,
+         "starting_pos": (1152,0)
          })
     g_cipher_panels.append(
-        {'active_image': pygame.image.load("purple_plate_active.png"),
-         'inactive_image': pygame.image.load("purple_plate_inactive.png"),
-         'is_active': False,
-         'starting_pos': (1536,0)
+        {"active_image": pygame.image.load("purple_plate_active.png"),
+         "inactive_image": pygame.image.load("purple_plate_inactive.png"),
+         "is_active": False,
+         "past_state": False,
+         "starting_pos": (1536,0)
          })
 
+
+# IN pygame.display pygame_screen, IN g_cipher_panels single_panel
+# RETURN bool panel_was_activated
+def DrawCipherPanelAction(pygame_screen, single_panel):
+    panel_was_activated = False
+    panel_was_deactivated = False
+    if single_panel["past_state"] == False and single_panel["is_active"] == True:
+        panel_was_activated = True
+    elif single_panel["past_state"] == True and single_panel["is_active"] == False:
+        panel_was_deactivated = True
+
+    if panel_was_activated:
+        pygame_screen.blit(single_panel["active_image"], single_panel["starting_pos"])
+    elif panel_was_deactivated:
+        pygame_screen.blit(single_panel["inactive_image"], single_panel["starting_pos"])
+
+    single_panel["past_state"] = single_panel["is_active"]
+    return panel_was_activated
+    
+    
 # IN pygame.display pygame_screen
-def DrawCipherPanels(pygame_screen):  
+def DrawCipherPanels(pygame_screen):
+    play_success_sound = False
     for single_panel in g_cipher_panels:
-        if single_panel['is_active'] == True:
-            pygame_screen.blit(single_panel['active_image'], single_panel['starting_pos'])
-        else:
-            pygame_screen.blit(single_panel['inactive_image'], single_panel['starting_pos'])
+        panel_was_activated = DrawCipherPanelAction(pygame_screen, single_panel)
+        if panel_was_activated == True:
+            play_success_sound = True
+        
+    if play_success_sound == True:
+        g_sound_library["success"].play()
+        
     pygame.display.flip()
+
 
 # IN pygame.display pygame_screen
 def ResetCipherPanels(pygame_screen):
     for single_panel in g_cipher_panels:
-        single_panel['is_active'] = False
-        pygame_screen.blit(single_panel['inactive_image'], single_panel['starting_pos'])
+        single_panel["is_active"] = False
+        pygame_screen.blit(single_panel["inactive_image"], single_panel["starting_pos"])
     pygame.display.flip()
+
 
 # IN int position_number
 # RETURN bool is_active
@@ -83,6 +119,7 @@ def ReadAnswerMatrix(position_number):
         is_active = True
     GPIO.output(g_matrix_output_pins[position_number], GPIO.LOW)
     return is_active
+
 
 # IN pygame pygame
 # RETURN bool quit_requested
@@ -96,26 +133,26 @@ def CheckForQuit(pygame):
     return quit_requested
 
 
-
-     
 def main():
     pygame.init()
     pygame.display.set_caption("GS Summit Cipher")
+    pygame.mouse.set_visible(False)
     if g_fullscreen:
         screen = pygame.display.set_mode(g_screen_resolution, pygame.FULLSCREEN)
     else:
         screen = pygame.display.set_mode(g_screen_resolution)
 
     LoadCipherPanels()
+    LoadSounds()
     ResetCipherPanels(screen)
 
     quit_requested = False
     while quit_requested == False:
-        for answer_position in range(5):
-            g_cipher_panels[answer_position]['is_active'] = ReadAnswerMatrix(answer_position)
+        for answer_position in range(g_answer_count):
+            g_cipher_panels[answer_position]["is_active"] = ReadAnswerMatrix(answer_position)
             
         DrawCipherPanels(screen)
-        time.sleep(0.2)
+        #time.sleep(0.1) NOTE: (Adam) Might not need delay to stop GPIO hammering with pygame in play
         quit_requested = CheckForQuit(pygame)
 
     # NOTE: (Adam) Clean-up
